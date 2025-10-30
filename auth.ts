4149adb22be00f520b661e194b52f4f2149dfda9
Lydia Farncombe <lydia.farncombe@adventa.com>
2025-10-30 21:27:57 +0000

    Add Amplify build config and clone auth profile

--- auth.ts
+++ auth.ts
@@ -11,7 +11,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
       },
       profile(profile) {
-        return profile;
+        return { ...profile };
       },
     }),
   ],
